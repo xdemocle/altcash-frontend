eaeed282596db62d3758960cc49eae9903bf026a
xdemocle <xdemocle@gmail.com>
2018-07-12 02:33:54 +0200

    adding coinsList orderBy to query; refactor CoinsList component

--- src/components/Coins/CoinsList.js
+++ src/components/Coins/CoinsList.js
@@ -1,59 +1,68 @@
 /* eslint-disable no-debugger */
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import compose from 'recompose/compose'
 import List from '@material-ui/core/List'
-import { graphql } from 'react-apollo'
+import Button from '@material-ui/core/Button'
+import Icon from '@material-ui/core/Icon'
+import { Query } from 'react-apollo'
 import CoinItem from './CoinItem'
-import { GET_COINS_LIST_WITH_SYMBOLS } from '../../graphql/queries.js'
+import { GET_COINS_LIST_WITH_MARKETS } from '../../graphql/queries.js'
 
 const styles = theme => ({
   root: {
     padding: theme.spacing.unit * 3
   },
   primaryText: {
-    color: theme.palette.primary.main
+    color: theme.palette.primary.main,
+    marginTop: 0
+  },
+  fab: {
+    position: 'absolute',
+    top: theme.spacing.unit * 3,
+    right: theme.spacing.unit * 3
   }
 })
 
-class CoinsList extends Component {
-  state = {
-    dense: false
-  }
+const CoinsList = ({ classes }) => {
+  let refetchFn = null
 
-  render() {
-    const { coins, classes } = this.props
-    const { dense } = this.state
+  return (
+    <div className={classes.root}>
+      <h1 className={classes.primaryText}>Coins available</h1>
+      <Button
+        mini
+        variant="fab"
+        color="primary"
+        className={classes.fab}
+        aria-label="Refetch coin list"
+        onClick={() => refetchFn()}
+      >
+        <Icon>refresh</Icon>
+      </Button>
+      <Query query={GET_COINS_LIST_WITH_MARKETS} notifyOnNetworkStatusChange>
+        {({ loading, error, data, refetch, networkStatus }) => {
+          refetchFn = refetch
+          if (loading || networkStatus === 4) return 'Loading coins list...'
+          if (error) return `Error! ${error.message}`
 
-    return (
-      <div className={classes.root}>
-        <h1 className={classes.primaryText}>Coins available</h1>
-        <List dense={dense}>
-          {coins.map((coin) => (
-            <CoinItem key={coin.id} coin={coin} />
-          ))}
-        </List>
-      </div>
-    )
-  }
+          return (
+            <React.Fragment>
+              <List>
+                {data.allCoins.map((coin) => (
+                  <CoinItem key={coin.id} coin={coin} />
+                ))}
+              </List>
+            </React.Fragment>
+          )
+        }}
+      </Query>
+    </div>
+  )
 }
 
 CoinsList.propTypes = {
-  classes: PropTypes.object.isRequired,
-  coins: PropTypes.array.isRequired
+  classes: PropTypes.object.isRequired
 }
 
-const CoinsListWithData = compose(
-  withStyles(styles, { withTheme: true }),
-  graphql(GET_COINS_LIST_WITH_SYMBOLS, {
-    props: ({ data }) => {
-      if (data.loading || data.error) return { coins: [] }
-      return {
-        coins: data.allCoins
-      }
-    }
-  })
-)(CoinsList)
-
-export default CoinsListWithData
+export default withStyles(styles, { withTheme: true })(CoinsList)
