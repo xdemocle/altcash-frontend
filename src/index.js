f112fab543e51ccad0a95111fc77b011baf205ad
xdemocle <xdemocle@gmail.com>
2019-08-03 03:52:04 +0200

    activate service worker

--- src/index.js
+++ src/index.js
@@ -33,4 +33,8 @@ if (module.hot) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+if (process.env.NODE_ENV === 'production') {
+  serviceWorker.register()
+} else {
+  serviceWorker.unregister()
+}
