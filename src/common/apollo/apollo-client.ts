ef7d1895d07eff465a7ecb6b5976413c121be60a
xdemocle <xdemocle@gmail.com>
2021-02-22 20:28:36 +0200

    add suffix for API calls

--- src/common/apollo/apollo-client.ts
+++ src/common/apollo/apollo-client.ts
@@ -4,11 +4,13 @@
 import { ApolloClient } from '@apollo/client'
 import { cache } from './apollo-cache'
 
+const ROOT_URL =
+  process.env.NODE_ENV !== 'development' ? '/altcash-fullstack' : ''
+
 // Initialize Apollo client with cache and state
 export const apolloClient = new ApolloClient({
   cache,
-  // uri: 'http://localhost:4000/graphql',
-  uri: '/graphql',
+  uri: `${ROOT_URL}/graphql`,
   // headers: {
   //   authorization: localStorage.getItem('token') || '',
   //   'client-name': 'Space Explorer [web]',
