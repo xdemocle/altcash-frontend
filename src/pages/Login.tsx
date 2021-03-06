6fb1cb8f318ccd40dc544ebeb770f007aa45c136
xdemocle <xdemocle@gmail.com>
2021-03-06 19:53:25 +0200

    skeletons login and signup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,33 +1,85 @@
+import { Button, TextField } from '@material-ui/core'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+
+type Inputs = {
+  email: string
+  username: string
+  password: string
+}
 
 const Login: React.FC = () => {
   const classes = useStyles()
+  const { register, handleSubmit, errors } = useForm<Inputs>({
+    // mode: 'onSubmit',
+    // reValidateMode: 'onChange',
+    // defaultValues: {},
+    // resolver: undefined,
+    // context: undefined,
+    criteriaMode: 'all',
+    shouldFocusError: true
+  })
+
+  const onSubmitHandler = (data: unknown) => {
+    // e.preventDefault()
+    console.log(data)
+  }
 
   return (
     <div className={classes.root}>
       <h1 className="display-3">Login</h1>
+      <form
+        noValidate
+        autoComplete="off"
+        method="POST"
+        onSubmit={handleSubmit(onSubmitHandler)}
+      >
+        <TextField
+          name="email"
+          label="Username / E-Mail"
+          autoComplete="email"
+          variant="outlined"
+          required
+          className={classes.textField}
+          inputRef={register({ required: true, maxLength: 320 })}
+          inputProps={{
+            maxLength: '320'
+          }}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          type="password"
+          autoComplete="password"
+          variant="outlined"
+          required
+          className={classes.textField}
+          inputRef={register({
+            required: true,
+            maxLength: 255,
+            minLength: {
+              value: 8,
+              message: 'Password must have at least 8 characters'
+            }
+          })}
+          inputProps={{
+            maxLength: '255'
+          }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          className={classes.buttonField}
+        >
+          Login
+        </Button>
+        {errors && console.log(errors)}
+      </form>
       <p>
-        Candy canes lemon drops cake chocolate caramels biscuit cake
-        gingerbread. Icing pie macaroon chupa chups gummies tart sugar plum bear
-        claw. Gummi bears sweet roll pudding.
-      </p>
-      <p>
-        Dragée fruitcake caramels liquorice sweet. Tiramisu soufflé lemon drops
-        chupa chups halvah toffee pastry. Chocolate bar brownie caramels
-        caramels candy canes. Marshmallow sweet roll chocolate bar dragée oat
-        cake.
-      </p>
-      <p>
-        Candy canes lemon drops cake chocolate caramels biscuit cake
-        gingerbread. Icing pie macaroon chupa chups gummies tart sugar plum bear
-        claw. Gummi bears sweet roll pudding.
-      </p>
-      <p>
-        Dragée fruitcake caramels liquorice sweet. Tiramisu soufflé lemon drops
-        chupa chups halvah toffee pastry. Chocolate bar brownie caramels
-        caramels candy canes. Marshmallow sweet roll chocolate bar dragée oat
-        cake.
+        Or <Link to="/signup">register here</Link> for a new account.
       </p>
     </div>
   )
@@ -38,5 +90,14 @@ export default Login
 const useStyles = makeStyles(({ spacing }: Theme) => ({
   root: {
     padding: spacing(3)
+  },
+  textField: {
+    marginRight: spacing(3),
+    marginBottom: spacing(3)
+  },
+  buttonField: {
+    paddingLeft: spacing(5),
+    paddingRight: spacing(5),
+    height: '3.5rem'
   }
 }))
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,34 +1,120 @@
+import { Button, TextField } from '@material-ui/core'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+
+type Inputs = {
+  email: string
+  username: string
+  password: string
+  password2: string
+}
 
 const Signup: React.FC = () => {
   const classes = useStyles()
+  const { register, handleSubmit, errors } = useForm<Inputs>({
+    // mode: 'onSubmit',
+    // reValidateMode: 'onChange',
+    // defaultValues: {},
+    // resolver: undefined,
+    // context: undefined,
+    criteriaMode: 'all',
+    shouldFocusError: true
+  })
+
+  const onSubmitHandler = (data: unknown) => {
+    // e.preventDefault()
+    console.log(data)
+  }
 
   return (
     <div className={classes.root}>
       <h1 className="display-3">Signup</h1>
-      <p>
-        Candy canes lemon drops cake chocolate caramels biscuit cake
-        gingerbread. Icing pie macaroon chupa chups gummies tart sugar plum bear
-        claw. Gummi bears sweet roll pudding.
-      </p>
-      <p>
-        Dragée fruitcake caramels liquorice sweet. Tiramisu soufflé lemon drops
-        chupa chups halvah toffee pastry. Chocolate bar brownie caramels
-        caramels candy canes. Marshmallow sweet roll chocolate bar dragée oat
-        cake.
-      </p>
-      <p>
-        Candy canes lemon drops cake chocolate caramels biscuit cake
-        gingerbread. Icing pie macaroon chupa chups gummies tart sugar plum bear
-        claw. Gummi bears sweet roll pudding.
-      </p>
-      <p>
-        Dragée fruitcake caramels liquorice sweet. Tiramisu soufflé lemon drops
-        chupa chups halvah toffee pastry. Chocolate bar brownie caramels
-        caramels candy canes. Marshmallow sweet roll chocolate bar dragée oat
-        cake.
-      </p>
+      <form
+        noValidate
+        autoComplete="off"
+        method="POST"
+        onSubmit={handleSubmit(onSubmitHandler)}
+      >
+        <TextField
+          name="email"
+          label="E-Mail"
+          variant="outlined"
+          required
+          className={classes.textField}
+          inputRef={register({
+            required: true,
+            maxLength: 320,
+            pattern: {
+              value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: 'E-Mail field has an error'
+            }
+          })}
+          inputProps={{
+            maxLength: '320'
+          }}
+        />
+        <TextField
+          name="username"
+          label="Username"
+          variant="outlined"
+          required
+          className={classes.textField}
+          inputRef={register({ required: true, maxLength: 25 })}
+          inputProps={{
+            maxLength: '25'
+          }}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          type="password"
+          autoComplete="new-password"
+          variant="outlined"
+          required
+          className={classes.textField}
+          inputRef={register({
+            required: true,
+            maxLength: 255,
+            minLength: {
+              value: 8,
+              message: 'Password must have at least 8 characters'
+            }
+          })}
+          inputProps={{
+            maxLength: '255'
+          }}
+        />
+        <TextField
+          name="password2"
+          label="Confirm Password"
+          type="password"
+          autoComplete="new-password"
+          variant="outlined"
+          required
+          className={classes.textField}
+          inputRef={register({
+            required: true,
+            maxLength: 255,
+            minLength: {
+              value: 8,
+              message: 'Password must have at least 8 characters'
+            }
+          })}
+          inputProps={{
+            maxLength: '255'
+          }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          className={classes.buttonField}
+        >
+          Register
+        </Button>
+        {errors && console.log(errors)}
+      </form>
     </div>
   )
 }
@@ -38,5 +124,14 @@ export default Signup
 const useStyles = makeStyles(({ spacing }: Theme) => ({
   root: {
     padding: spacing(3)
+  },
+  textField: {
+    marginRight: spacing(3),
+    marginBottom: spacing(3)
+  },
+  buttonField: {
+    paddingLeft: spacing(5),
+    paddingRight: spacing(5),
+    height: '3.5rem'
   }
 }))
