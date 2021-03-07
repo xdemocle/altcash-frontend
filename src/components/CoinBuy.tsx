8a5058bbdf77e17ea31551f49fe840eef7360465
xdemocle <xdemocle@gmail.com>
2021-03-07 14:45:29 +0200

    complete skeleton buy widget

--- src/components/CoinBuy.tsx
+++ src/components/CoinBuy.tsx
@@ -1,12 +1,22 @@
-import { Card, Grid, TextField } from '@material-ui/core'
+import { Button, Card, Grid, TextField } from '@material-ui/core'
+import Hidden from '@material-ui/core/Hidden'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import InputLabel from '@material-ui/core/InputLabel'
 import green from '@material-ui/core/colors/green'
 import grey from '@material-ui/core/colors/grey'
 import { makeStyles } from '@material-ui/core/styles'
+import SwapHorizIcon from '@material-ui/icons/SwapHoriz'
+import SwapVertIcon from '@material-ui/icons/SwapVert'
+import classnames from 'classnames'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import NumberFormat from 'react-number-format'
 import { Coin } from './CoinItem'
+
+interface Inputs {
+  localCurrency: string
+  cryptoCurrency: string
+}
 
 interface NumberFormatCustomProps {
   inputRef: (instance: NumberFormat | null) => void
@@ -42,20 +52,44 @@ interface Props {
 
 const CoinBuy: React.FC<Props> = ({ coin }: Props) => {
   const classes = useStyles()
+  const { register, handleSubmit, errors } = useForm<Inputs>({
+    // mode: 'onSubmit',
+    // reValidateMode: 'onChange',
+    // defaultValues: {},
+    // resolver: undefined,
+    // context: undefined,
+    criteriaMode: 'all',
+    shouldFocusError: true
+  })
+
+  const onSubmitHandler = (data: unknown) => {
+    // e.preventDefault()
+    console.log(data)
+  }
 
   return (
     <Card className={classes.root}>
-      <form noValidate autoComplete="off">
+      <form
+        noValidate
+        autoComplete="off"
+        method="POST"
+        onSubmit={handleSubmit(onSubmitHandler)}
+      >
         <Grid container>
-          <Grid item xs={12} md={6} className={classes.gridLeft}>
+          <Grid item xs={12} md={4} className={classes.grid}>
             <InputLabel htmlFor="gridLeftInput" className={classes.gridTitle}>
-              You pay in Rands
+              You pay in <strong>Rands (ZAR)</strong>
             </InputLabel>
             <TextField
               id="gridLeftInput"
+              name="localCurrency"
               fullWidth
-              // helperText="ZAR"
+              helperText={errors.localCurrency && errors.localCurrency.message}
               variant="outlined"
+              inputRef={register({ required: true, maxLength: 25 })}
+              inputProps={{
+                maxLength: '25'
+              }}
               InputProps={{
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 inputComponent: NumberFormatCustom as any,
@@ -65,15 +99,37 @@ const CoinBuy: React.FC<Props> = ({ coin }: Props) => {
               }}
             />
           </Grid>
-          <Grid item xs={12} md={6} className={classes.gridRight}>
+
+          <Grid item xs={12} md={1} className={classes.grid}>
+            <div className={classes.flex}>
+              <Hidden smDown>
+                <SwapHorizIcon color="primary" className={classes.arrow} />
+              </Hidden>
+              <Hidden mdUp>
+                <SwapVertIcon
+                  color="primary"
+                  className={classnames(classes.arrow, classes.arrowMobile)}
+                />
+              </Hidden>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} md={4} className={classes.grid}>
             <InputLabel htmlFor="gridRightInput" className={classes.gridTitle}>
               You get <strong className={classes.symbol}>{coin.name}</strong>
             </InputLabel>
             <TextField
               id="gridRightInput"
+              name="cryptoCurrency"
               fullWidth
-              // helperText={coin.symbol}
+              helperText={
+                errors.cryptoCurrency && errors.cryptoCurrency.message
+              }
               variant="outlined"
+              inputRef={register({ required: true, maxLength: 25 })}
+              inputProps={{
+                maxLength: '25'
+              }}
               InputProps={{
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 inputComponent: NumberFormatCustom as any,
@@ -84,6 +140,19 @@ const CoinBuy: React.FC<Props> = ({ coin }: Props) => {
                 )
               }}
             />
+          </Grid>
+
+          <Grid item xs={12} md={3}>
+            <div className={classes.buyButtonInner}>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                className={classes.buyButton}
+              >
+                Buy Now
+              </Button>
+            </div>
           </Grid>
         </Grid>
       </form>
@@ -100,17 +169,11 @@ const useStyles = makeStyles(({ breakpoints, typography, spacing }) => ({
     //   padding: typography.pxToRem(spacing(2))
     // }
   },
-  gridLeft: {
-    paddingRight: typography.pxToRem(spacing(1)),
+  grid: {
+    paddingRight: typography.pxToRem(spacing(2)),
     [breakpoints.down('sm')]: {
       paddingRight: 0,
       marginBottom: typography.pxToRem(spacing(3))
-    }
-  },
-  gridRight: {
-    paddingLeft: typography.pxToRem(spacing(1)),
-    [breakpoints.down('sm')]: {
-      paddingLeft: 0
     }
   },
   gridTitle: {
@@ -121,5 +184,33 @@ const useStyles = makeStyles(({ breakpoints, typography, spacing }) => ({
   },
   symbol: {
     color: green[700]
+  },
+  buyButton: {
+    marginTop: '1.4rem',
+    minHeight: '3.35rem',
+    width: '100%'
+  },
+  buyButtonInner: {
+    marginLeft: 0,
+    [breakpoints.up('md')]: {
+      marginLeft: typography.pxToRem(spacing(2))
+    }
+  },
+  flex: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    [breakpoints.up('md')]: {
+      paddingTop: '1rem'
+    }
+  },
+  arrow: {
+    width: '2.5rem',
+    height: '2.5rem'
+  },
+  arrowMobile: {
+    width: '4.5rem',
+    height: '4.5rem'
   }
 }))
