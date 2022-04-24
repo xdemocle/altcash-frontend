67e9512ad9f010335752ee4290689eb116b5f522
xdemocle <xdemocle@gmail.com>
2022-04-24 13:28:01 +0200

    adding favicon and fix warning

--- server.ts
+++ server.ts
@@ -1,13 +1,13 @@
-import express from 'express'
-import fallback from 'express-history-api-fallback'
-import { createProxyMiddleware } from 'http-proxy-middleware'
+import express from 'express';
+import fallback from 'express-history-api-fallback';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-const app = express()
+const app = express();
 
-const graphqlApiUri = process.env.REACT_APP_API_URL || 'http://localhost:4000/'
+const graphqlApiUri = process.env.REACT_APP_API_URL || 'http://localhost:4000/';
 
-const root = `${__dirname}/build`
-app.use(express.static(root))
+const root = `${__dirname}/build`;
+app.use(express.static(root));
 
 // add proxy
 app.use(
@@ -16,12 +16,12 @@ app.use(
     target: graphqlApiUri,
     changeOrigin: true
   })
-)
+);
 
 // history fallback
-app.use(fallback('index.html', { root }))
+app.use(fallback('index.html', { root }));
 
-const port = process.argv[2] // get port from command line argument
+const port = process.argv[2]; // get port from command line argument
 
 // eslint-disable-next-line no-console
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
