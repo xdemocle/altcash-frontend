f747018323c6d1d87d986dda8ebaac351bd5b351
xdemocle <xdemocle@gmail.com>
2022-08-30 14:42:40 +0200

    fix for production

--- src/common/apollo/apollo-client.ts
+++ src/common/apollo/apollo-client.ts
@@ -15,14 +15,15 @@ if (!isServer()) {
   window.addEventListener('online', () => offlineLink.open());
 }
 
+const uri =
+  process.env.NODE_ENV !== 'development'
+    ? 'https://altcash.vercel.app/graphql'
+    : process.env.NEXT_PUBLIC_GRAPHQL_SERVER + '/graphql';
+
 // Initialize Apollo client with cache and state
 export const apolloClient = new ApolloClient({
   ssrMode: isServer(),
   cache,
   connectToDevTools: true,
-  link: ApolloLink.from([
-    new RetryLink(),
-    offlineLink,
-    new HttpLink({ uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER + '/graphql' })
-  ])
+  link: ApolloLink.from([new RetryLink(), offlineLink, new HttpLink({ uri })])
 });
